1bed293a6d7a13ad01521897e4acbef232b0259b
John Economou <johnlafayeti@gmail.com>
2022-07-28 20:32:59 -0400

    simple query works

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const mongo = require('mongodb').MongoClient;
 
 var url = 'mongodb://localhost:27017/SampleDb';
+var reviews;
+var characteristics;
 
 mongo.connect(url, (err, client) => {
   if (err) {
@@ -10,8 +12,15 @@ mongo.connect(url, (err, client) => {
   }
   console.log('Connected successfully to server')
 
-  const reviews = client.db('reviews').collection('reviewPhotos');
-  const characteristics = client.db('reviews').collection('mergedCharacteristics');
+  reviews = client.db('reviews').collection('reviewPhotos');
+  characteristics = client.db('reviews').collection('mergedCharacteristics');
 });
 
 
+let findReviews = function (productId) {
+  console.log('productId', productId);
+  // console.log('test query', reviews.find({product_id: productId}))
+  return (reviews.find({product_id: productId}).toArray());
+}
+
+module.exports.findReviews = findReviews;
--- server.js
+++ server.js
@@ -2,13 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-let db = require('./index.js');
-
+// let db = require('./index.js');
+const mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/reviews';
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/reviews/:id', (req, res) => {
-  res.send('reviews ' + req.params.id + ' count ' + req.query.count + ' sort ' + req.query.sort)
+mongo.connect(url, (err, client) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Connected successfully to server')
+  const reviews = client.db('reviews').collection('reviewPhotos');
+  console.log('reviews', reviews);
+  // const characteristics = client.db('reviews').collection('mergedCharacteristics');
 
+  app.get('/reviews/:id', (req, res) => {
+    let prodId = parseInt(req.params.id);
+    console.log('param', typeof req.params.id);
+    reviews.find({ product_id: prodId }).toArray((err, docs) => {
+      console.log('params inside find', req.params.id);
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(docs);
+      }
+    })
+  })
 });
 
 app.listen(port, () => {
