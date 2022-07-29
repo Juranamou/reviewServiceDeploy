51ef31d25c9bbc613b23dc2b4eda79f6a2e14459
John Economou <johnlafayeti@gmail.com>
2022-07-29 19:38:35 -0400

    building meta

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const mongo = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/SampleDb';
+var url = 'mongodb://localhost:27017';
 var reviews;
 var characteristics;
 
@@ -11,15 +11,33 @@ mongo.connect(url, (err, client) => {
     return;
   }
   console.log('Connected successfully to server');
-  reviews = client.db('reviews').collection('reviewPhotos');
-  characteristics = client.db('reviews').collection('mergedCharacteristics');
+  reviews = client.db('reviews').collection('everything');
 });
 
 
 let findReviews = function (productId) {
   console.log('productId', productId);
   // console.log('test query', reviews.find({product_id: productId}))
-  return (reviews.find({product_id: productId}).toArray());
+  return (reviews.find({productId: productId}).toArray());
+}
+
+let buildReviewObject = function (reviewData) {
+  // console.log('review data')
+  let reviewObj = {
+    product: reviewData[0].productId,
+    results: reviewData
+  };
+  return reviewObj;
+}
+
+let buildMetaObject = function (reviewData) {
+  console.log('review data', reviewData);
+  let metaObj = {
+    test: 'test'
+  };
+  return metaObj;
 }
 
 module.exports.findReviews = findReviews;
+module.exports.buildReviewObject = buildReviewObject;
+module.exports.buildMetaObject = buildMetaObject;
--- server.js
+++ server.js
@@ -9,8 +9,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/reviews/:id', (req, res) => {
   let prodId = parseInt(req.params.id);
   db.findReviews(prodId)
+    .then((data) => { return db.buildReviewObject(data); })
     .then((data) => { res.send(data).status(200) })
-    .catch(() => {res.sendStatus(500)});
+    .catch(() => { res.sendStatus(500) });
+})
+
+app.get('/meta/:id', (req, res) => {
+  console.log('meta')
+  let prodId = parseInt(req.params.id);
+  db.findReviews(prodId)
+    .then((data) => { return db.buildMetaObject(data); })
+    .then((data) => { res.send(data).status(200) })
+    .catch(() => { res.sendStatus(500) });
 })
 
 app.listen(port, () => {
